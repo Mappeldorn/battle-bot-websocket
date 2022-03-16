3da3da97d8128ad616b6f9f8e29ed88ec0e1aba3
Max Appeldorn <mappeldorn1@st.noorderpoort.nl>
2022-03-16 14:15:56 +0100

    clean-up

--- src/server.js
+++ src/server.js
@@ -1,20 +1,11 @@
 const { createServer } = require("http");
 const express = require("express");
-const cors = require('cors');
 const WebSocket = require("ws");
 
 
 const app = express();
 
 app.use(express.json({ extended: false }));
-
-// app.use(cors());
-// app.use(function (req, res, next) {
-//     res.header("Acces-Control-Allow-Origin", "*");
-//     res.header("Acces-Control-Allow-Headers", 'Content-Type, Authorization', );
-//     res.header('Acces-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
-//     next();
-// });
 
 app.use("/websocket/chat", require("./routes/websocket/chat"));
 
